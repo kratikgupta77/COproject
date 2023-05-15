284229a0d2e158856bbe47f72c8e052a6e607127
Devesh-Hooda <122511434+Devesh-Hooda@users.noreply.github.com>
2023-05-15 19:00:22 +0530

    Update assembler.py

--- assembler.py
+++ assembler.py
@@ -1,26 +1,5 @@
 import sys
 
-opcodeTable={      "add": "00000",
-                    "sub": "00001",
-                    "mov": ["00010", "00011"], 
-                    "ld" : "00100",
-                    "st" : "00101",
-                    "mul": "00110",
-                    "div": "00111",
-                    "rs" : "01000",
-                    "ls" : "01001",
-                    "xor": "01010",
-                    "or" : "01011",
-                    "and": "01100",
-                    "not": "01101",
-                    "cmp": "01110",
-                    "jmp": "01111",
-                    "jlt": "11100",
-                    "jgt": "11101",
-                    "je" : "11111",
-                    "hlt": "11010"
-		}
-
 def dec_to_bin(dec):
    
 def opcode(instruction):
@@ -29,17 +8,17 @@ def instrct_type():
 
 def registerAddress(register):
 
-varLines = 0
 assemblyCode = []
 for line in sys.stdin:
     assemblyCode.append(line)
 
-convertedBinary = []
-encounteredErrors = []
+convertedBinary = []  
+encounteredErrors = [] 
 labels = {}
+encounteredErrors = []
 variables = {}
 lineNumber = 0
-haltEncountered = False
+varLines = 0
 
 for _ in range(varLines):
     if not assemblyCode[0]:
@@ -52,17 +31,21 @@ for _ in range(varLines):
         if line[1] not in variables.keys():
             variables[line[1]] = dec_to_bin()
             assemblyCode.pop(0)
+            varLines+= 1
             number += 1
             continue
         else:
             number += 1
+            varLines += 1
             continue
     elif line[0] == "var":
         assemblyCode.pop(0)
         number += 1
+        varLines += 1
         continue
     else:
         break
+
 
 for i, line in enumerate(assemblyCode, 1):
     if not line:
@@ -74,7 +57,7 @@ for i, line in enumerate(assemblyCode, 1):
             encounteredErrors.append(f"ERROR at Line {i + varLines}: Multiple declarations for the same label found")
             continue
         else:
-            labels[label] = dec_to_bin(i - 1)
+            labels[label] = dec_to_bin(i-1)
             line = rest_of_line.strip()
 
     currentLine = line.split()
@@ -85,26 +68,140 @@ if currentLine[0] == "mov":
         reg2 = registerAddress(currentLine[2])
         if reg1 != -1 and reg2 != -1:
             if reg1 == "111":
-                encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Illegal use of FLAGS")
-                continue
+                encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Wrong FLAGS")
+               
             convertedBinary.append(opcode(currentLine[0], 1) + "00000" + reg1 + reg2)
-            continue
+           
         elif currentLine[2][1:].isdecimal():
             if currentLine[2][0] != "$":
                 encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Syntax Error")
-                continue
+                
             if reg1 == -1:
-                encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Invalid Register")
-                continue
+                encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Invalid Reg")
+               
             if reg1 == "111":
-                encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Illegal use of FLAGS")
-                continue
+                encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Wrong FLAGS")
+                
             imm_value = int(currentLine[2][1:])
             if imm_value < 0 or imm_value > 255:
                 encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Illegal Immediate Value")
-                continue
-            convertedBinary.append(opcode(currentLine[0], 0) + reg1 + binary8bit(imm_value))
-            continue
+             
+            convertedBinary.append(opcode(currentLine[0], 0) + reg1 + dec_to_bin(imm_value))
+          
     encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Wrong Syntax used for instruction")
-    continue
+    
+
+
+if instrct_type(currentLine[0]) == 'a':
+    if len(currentLine) == 4:
+        reg1 = registerAddress(currentLine[1])
+        reg2 = registerAddress(currentLine[2])
+        reg3 = registerAddress(currentLine[3])
+        if reg1 == -1 or reg2 == -1 or reg3 == -1:
+            encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Invalid Register")
+        if reg1 == "111" or reg2 == "111" or reg3 == "111":
+            encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Illegal use of FLAGS")
+        convertedBinary.append(opcode(currentLine[0]) + "00" + reg1 + reg2 + reg3)
+        
+    encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Wrong Syntax used for instruction")
+    
+
+
+if instrct_type(currentLine[0]) == 'b':
+    if len(currentLine) == 3:
+        immediate_value = int(currentLine[2][1:]) if currentLine[2][0] == "$" else None
+        if immediate_value is None:
+            encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Syntax Error")
+            
+        if immediate_value not in range(0, 256):
+            encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Illegal Immediate Value")
+            
+        reg = registerAddress(currentLine[1])
+        if reg == -1:
+            encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Invalid Register")
+            
+        if reg == "111":
+            encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Illegal use of FLAGS")
+            
+        convertedBinary.append(opcode(currentLine[0]) + reg + dec_to_bin(immediate_value))
+        
+    encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Wrong Syntax used for instruction")
+    
+
+
+if instrct_type(currentLine[0]) == 'c':
+    if len(currentLine) == 3:
+        reg1 = registerAddress(currentLine[1])
+        reg2 = registerAddress(currentLine[2])
+        if all(reg == -1 for reg in (reg1, reg2)):
+            encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Invalid Register")
+            
+        if any(reg == "111" for reg in (reg1, reg2)):
+            encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Illegal use of FLAGS")
+            
+        convertedBinary.append(opcode(currentLine[0]) + "00000" + reg1 + reg2)
+        
+    encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Wrong Syntax used for instruction")
+    
+
+if instrct_type(currentLine[0]) == 'd':
+    if len(currentLine) == 3:
+        reg = registerAddress(currentLine[1])
+        if reg == -1:
+            encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Invalid Register")
+           
+        if reg == "111":
+            encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Illegal use of FLAGS")
+           
+        variable = variables.get(currentLine[2])
+        if variable is None:
+            if currentLine[2] in labels.keys():
+                encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Misuse of label as variable")
+            else:
+                encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Use of undefined variable")
+           
+        convertedBinary.append(opcode(currentLine[0]) + reg + variable)
+       
+    encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Wrong Syntax used for instruction")
+   
+
+
+if instrct_type(currentLine[0]) == 'e':
+    if len(currentLine) == 2:
+        ins = currentLine.pop(0)
+        label = currentLine[0]
+        if label not in labels.keys():
+            if label in variables.keys():
+                encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Misuse of variable as label")
+            else:
+                encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Use of undefined labels")
+            
+        else:
+            convertedBinary.append(opcode(ins) + '0' * 3 + labels[label])
+            
+    encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Wrong Syntax used for instruction")
+    
+
+
+if instrct_type(currentLine[0]) == 'f':
+    if len(currentLine) == 1:
+        convertedBinary.append(opcode(currentLine[0]) + '0' * 11)
+        HltCalled = True
+        
+    encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Wrong Syntax used for instruction")
+    
+
+
+if not HltCalled:
+    encounteredErrors.append("ERROR: No halt (hlt) instruction found")
+
+if len(convertedBinary) > 256:
+    print("ERROR: The code length exceeds the maximum capacity (256 lines)")
+else:
+    if encounteredErrors:
+        for error in encounteredErrors:
+            print(error)
+    else:
+        output = '\n'.join(convertedBinary)
+        print(output)
 
