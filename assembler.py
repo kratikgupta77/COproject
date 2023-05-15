576b69d6d96c01a826d740da11d02dcabbe9b98f
Devesh-Hooda <122511434+Devesh-Hooda@users.noreply.github.com>
2023-05-15 21:13:33 +0530

    Update assembler.py

--- assembler.py
+++ assembler.py
@@ -1,207 +1,217 @@
-import sys
+regs = {
+     'R0':'000',
+     'R1':'001',
+     'R2':'010',
+     'R3':'011',
+     'R4':'100',
+     'R5':'101',
+     'R6':'110',
+     'FLAGS':'111'
+     }
+     
+opcodeTable={   "var": "",
+                "add": "0000000",
+                "sub": "0000100",
+                "mov": ["000100", "0001100000"], 
+                "ld" : "001000",
+                "st" : "001010",
+                "mul": "0011000",
+                "div": "0011100000",
+                "rs" : "010000",
+                "ls" : "010010",
+                "xor": "0101000",
+                "or" : "0101100",
+                "and": "0110000",
+                "not": "0110100000",
+                "cmp": "0111000000",
+                "jmp": "011110000",
+                "jlt": "111000000",
+                "jgt": "111010000",
+                "je" : "111110000",
+                "hlt": "1101000000000000"
+}
 
-def dec_to_bin(dec):
-   
-def opcode(instruction):
+def dec_to_bin(n):
+    i=0
+    num=0
+    while(n!=0):
+        num+=(n%2)(10*i)
+        n=n//2
+        i+=1
+    n_num=str(num).zfill(8)
+    return n_num
 
-def instrct_type():
-
-def registerAddress(register):
-
-assemblyCode = []
+Out = []
+out_index = 0
+Var_count = 0
+Errors = 0
+Source = []
 for line in sys.stdin:
-    assemblyCode.append(line)
+    Source.append(line)
 
-convertedBinary = []  
-encounteredErrors = [] 
-labels = {}
-encounteredErrors = []
-variables = {}
-lineNumber = 0
-varLines = 0
-
-for _ in range(varLines):
-    if not assemblyCode[0]:
-        number += 1
-        continue
-
-    line = assemblyCode[0].split()
-    number = 1
-    if line[0] == "var" and len(line) == 2:
-        if line[1] not in variables.keys():
-            variables[line[1]] = dec_to_bin()
-            assemblyCode.pop(0)
-            varLines+= 1
-            number += 1
-            continue
+for i in range(len(Source)):
+    linenum = i+1
+    labels = []
+    
+    curr = Source[i].split()
+    if curr[0][::-1] == ":":
+        label , rest = curr.split(:)
+        labels.append(label)
+        
+        newcurr = rest.split()
+        
+        
+        if newcurr[0] in opcodeTable.keys() :
+            if newcurr[0] == "add" or newcurr[0] == "sub" or newcurr[0] == "mul" or newcurr[0] == "xor" or newcurr[0] == "or" or newcurr[0] == "and":
+                if length(newcurr) != 3:
+                    Errors += 1
+                elif newcurr[1] or newcurr[2] or newcurr[3] not in regs.keys():
+                    Errors += 1
+                else : 
+                    Out[out_index] = opcodeTable[newcurr[0]],regs[newcurr[1]],regs[newcurr[2]],regs[newcurr[2]]
+                out_index += 1
+                i += 1
+            
+            elif newcurr[0] == "mov":
+                tempval = 0
+                if newcurr[2][0] == "$":
+                    opcodeTable[newcur[0][0]] = tempval
+                    if len(newcurr) != 2:
+                        Errors += 1
+                    else:
+                        Out[out_index] = tempval,regs[newcurr[1]],dec_to_bin(int(newcurr[2][1::]))
+                    out_index+=1
+                    i += 1
+                else :
+                    opcodeTable[newcur[0][1]] = tempval
+                    if len(newcurr) != 2:
+                        Errors += 1
+                    else:
+                        Out[out_index] = tempval,regs[newcurr[1]],regs[newcurr[2]]
+                    out_index+=1
+                    i += 1
+            
+            elif newcurr[0] == "rs" or newcurr[0] == "ls":
+                if len(newcurr) != 3:
+                    Errors += 1
+                else :
+                    Out[out_index] = opcodeTable[newcurr[0]],regs[newcurr[1]],regs[newcurr[2]]
+                out_index += 1
+                i += 1
+            
+            elif newcurr[0] == "div" or newcurr[0] == "not" or newcurr[0] == "cmp":
+                if len(newcurr) != 3 :
+                    Errors += 1 
+                else :
+                    Out[out_index] = opcodeTable[newcurr[0]],regs[newcurr[1]],regs[newcurr[2]]
+                out_index += 1 
+                i += 1
+                    
+            elif newcurr[0] == "ld" or newcurr[0] === "st":
+                if len(newcurr) != 3 :
+                    Errors += 1
+                elif newcurr[2][0] != "$" :
+                    Errors += 1
+                else :
+                    Out[out_index] = opcodeTable[newcurr[0]],regs[newcurr[1]],dec_to_bin(int(newcurr[2][1::]))
+                out_index += 1
+                i += 1
+            
+            elif newcurr[0] == "jmp" or newcurr[0] == "jlt" or newcurr[0] == "jgt" or newcurr[0] == "je":
+                if len(newcurr) != 2:
+                    Error += 1
+                elif newcurr[1][0] != "$":
+                    Error += 1
+                else :
+                    Out[out_index] = opcodeTable[newcurr[0]],dec_to_bin(int(newcurr[1][1::]))
+                out_index += 1
+                i += 1
+        
+    elif curr[0] in opcodeTable.keys() :
+        if curr[0] == "add" or curr[0] == "sub" or curr[0] == "mul" or curr[0] == "xor" or curr[0] == "or" or curr[0] == "and":
+            if length(curr) != 3:
+                Errors += 1
+            elif curr[1] or curr[2] or curr[3] not in regs.keys():
+                Errors += 1
+            else : 
+                Out[out_index] = opcodeTable[curr[0]],regs[curr[1]],regs[curr[2]],regs[curr[2]]
+            out_index += 1
+            i += 1
+        
+        elif curr[0] == "mov":
+            tempval = 0
+            if curr[2][0] == "$":
+                opcodeTable[newcur[0][0]] = tempval
+                if len(curr) != 2:
+                    Errors += 1
+                else:
+                    Out[out_index] = tempval,regs[curr[1]],dec_to_bin(int(curr[2][1::]))
+                out_index+=1
+                i += 1
+            else :
+                opcodeTable[newcur[0][1]] = tempval
+                if len(curr) != 2:
+                    Errors += 1
+                else:
+                    Out[out_index] = tempval,regs[curr[1]],regs[curr[2]]
+                out_index+=1
+                i += 1
+        
+        elif curr[0] == "rs" or curr[0] == "ls":
+            if len(curr) != 3:
+                Errors += 1
+            else :
+                Out[out_index] = opcodeTable[curr[0]],regs[curr[1]],regs[curr[2]]
+            out_index += 1
+            i += 1
+        
+        elif curr[0] == "div" or curr[0] == "not" or curr[0] == "cmp":
+            if len(curr) != 3 :
+                Errors += 1 
+            else :
+                Out[out_index] = opcodeTable[curr[0]],regs[curr[1]],regs[curr[2]]
+            out_index += 1 
+            i += 1
+                
+        elif curr[0] == "ld" or curr[0] === "st":
+            if len(curr) != 3 :
+                Errors += 1
+            elif curr[2][0] != "$" :
+                Errors += 1
+            else :
+                Out[out_index] = opcodeTable[curr[0]],regs[curr[1]],dec_to_bin(int(curr[2][1::]))
+            out_index += 1
+            i += 1
+        
+        elif curr[0] == "jmp" or curr[0] == "jlt" or curr[0] == "jgt" or curr[0] == "je":
+            if len(curr) != 2:
+                Error += 1
+            elif curr[1][0] != "$":
+                Error += 1
+            else :
+                Out[out_index] = opcodeTable[curr[0]],dec_to_bin(int(curr[1][1::]))
+            out_index += 1
+            i += 1
+        
+        elif curr[0] == "var":
+            Var_count += 1
+        out_index += 1
+        i += 1
+    
+    elif curr[0] == hlt:
+        if len(curr) != 1:
+            Errors += 1
         else:
-            number += 1
-            varLines += 1
-            continue
-    elif line[0] == "var":
-        assemblyCode.pop(0)
-        number += 1
-        varLines += 1
-        continue
-    else:
+            Out[out_index] = opcodeTable[curr[0]]
         break
 
 
-for i, line in enumerate(assemblyCode, 1):
-    if not line:
-        continue
-
-    if ':' in line:
-        label, rest_of_line = line.split(':', maxsplit=1)
-        if label in labels:
-            encounteredErrors.append(f"ERROR at Line {i + varLines}: Multiple declarations for the same label found")
-            continue
-        else:
-            labels[label] = dec_to_bin(i-1)
-            line = rest_of_line.strip()
-
-    currentLine = line.split()
-
-if currentLine[0] == "mov":
-    if len(currentLine) == 3:
-        reg1 = registerAddress(currentLine[1])
-        reg2 = registerAddress(currentLine[2])
-        if reg1 != -1 and reg2 != -1:
-            if reg1 == "111":
-                encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Wrong FLAGS")
-               
-            convertedBinary.append(opcode(currentLine[0], 1) + "00000" + reg1 + reg2)
-           
-        elif currentLine[2][1:].isdecimal():
-            if currentLine[2][0] != "$":
-                encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Syntax Error")
-                
-            if reg1 == -1:
-                encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Invalid Reg")
-               
-            if reg1 == "111":
-                encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Wrong FLAGS")
-                
-            imm_value = int(currentLine[2][1:])
-            if imm_value < 0 or imm_value > 255:
-                encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Illegal Immediate Value")
-             
-            convertedBinary.append(opcode(currentLine[0], 0) + reg1 + dec_to_bin(imm_value))
-          
-    encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Wrong Syntax used for instruction")
-    
-
-
-if instrct_type(currentLine[0]) == 'a':
-    if len(currentLine) == 4:
-        reg1 = registerAddress(currentLine[1])
-        reg2 = registerAddress(currentLine[2])
-        reg3 = registerAddress(currentLine[3])
-        if reg1 == -1 or reg2 == -1 or reg3 == -1:
-            encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Invalid Register")
-        if reg1 == "111" or reg2 == "111" or reg3 == "111":
-            encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Illegal use of FLAGS")
-        convertedBinary.append(opcode(currentLine[0]) + "00" + reg1 + reg2 + reg3)
         
-    encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Wrong Syntax used for instruction")
-    
 
-
-if instrct_type(currentLine[0]) == 'b':
-    if len(currentLine) == 3:
-        immediate_value = int(currentLine[2][1:]) if currentLine[2][0] == "$" else None
-        if immediate_value is None:
-            encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Syntax Error")
-            
-        if immediate_value not in range(0, 256):
-            encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Illegal Immediate Value")
-            
-        reg = registerAddress(currentLine[1])
-        if reg == -1:
-            encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Invalid Register")
-            
-        if reg == "111":
-            encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Illegal use of FLAGS")
-            
-        convertedBinary.append(opcode(currentLine[0]) + reg + dec_to_bin(immediate_value))
-        
-    encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Wrong Syntax used for instruction")
-    
-
-
-if instrct_type(currentLine[0]) == 'c':
-    if len(currentLine) == 3:
-        reg1 = registerAddress(currentLine[1])
-        reg2 = registerAddress(currentLine[2])
-        if all(reg == -1 for reg in (reg1, reg2)):
-            encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Invalid Register")
-            
-        if any(reg == "111" for reg in (reg1, reg2)):
-            encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Illegal use of FLAGS")
-            
-        convertedBinary.append(opcode(currentLine[0]) + "00000" + reg1 + reg2)
-        
-    encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Wrong Syntax used for instruction")
-    
-
-if instrct_type(currentLine[0]) == 'd':
-    if len(currentLine) == 3:
-        reg = registerAddress(currentLine[1])
-        if reg == -1:
-            encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Invalid Register")
-           
-        if reg == "111":
-            encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Illegal use of FLAGS")
-           
-        variable = variables.get(currentLine[2])
-        if variable is None:
-            if currentLine[2] in labels.keys():
-                encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Misuse of label as variable")
-            else:
-                encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Use of undefined variable")
-           
-        convertedBinary.append(opcode(currentLine[0]) + reg + variable)
-       
-    encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Wrong Syntax used for instruction")
    
-
-
-if instrct_type(currentLine[0]) == 'e':
-    if len(currentLine) == 2:
-        ins = currentLine.pop(0)
-        label = currentLine[0]
-        if label not in labels.keys():
-            if label in variables.keys():
-                encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Misuse of variable as label")
-            else:
-                encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Use of undefined labels")
-            
-        else:
-            convertedBinary.append(opcode(ins) + '0' * 3 + labels[label])
-            
-    encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Wrong Syntax used for instruction")
-    
-
-
-if instrct_type(currentLine[0]) == 'f':
-    if len(currentLine) == 1:
-        convertedBinary.append(opcode(currentLine[0]) + '0' * 11)
-        HltCalled = True
+   
+   
+   
+   
+   
         
-    encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Wrong Syntax used for instruction")
-    
-
-
-if not HltCalled:
-    encounteredErrors.append("ERROR: No halt (hlt) instruction found")
-
-if len(convertedBinary) > 256:
-    print("ERROR: The code length exceeds the maximum capacity (256 lines)")
-else:
-    if encounteredErrors:
-        for error in encounteredErrors:
-            print(error)
-    else:
-        output = '\n'.join(convertedBinary)
-        print(output)
-
