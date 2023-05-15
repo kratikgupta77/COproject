e19d65649e6be76b14fcb0c6ef7c9d14a0c3a20b
Devesh-Hooda <122511434+Devesh-Hooda@users.noreply.github.com>
2023-05-15 17:10:02 +0530

    Update assembler.py

--- assembler.py
+++ assembler.py
@@ -1,3 +1,5 @@
+import sys
+
 opcodeTable={      "add": "00000",
                     "sub": "00001",
                     "mov": ["00010", "00011"], 
@@ -17,17 +19,27 @@ opcodeTable={      "add": "00000",
                     "jgt": "11101",
                     "je" : "11111",
                     "hlt": "11010"
-}
-def dec_to_bin(n):
-    i=0
-    num=0
-    while(n!=0):
-        num+=(n%2)(10*i)
-        n=n//2
-        i+=1
-    n_num=str(num).zfill(8)
-    return n_num
-number = 1
+		}
+
+def dec_to_bin(dec):
+   
+def opcode(instruction):
+
+def instrct_type():
+
+def registerAddress(register):
+
+varLines = 0
+assemblyCode = []
+for line in sys.stdin:
+    assemblyCode.append(line)
+
+convertedBinary = []
+encounteredErrors = []
+labels = {}
+variables = {}
+lineNumber = 0
+haltEncountered = False
 
 for _ in range(varLines):
     if not assemblyCode[0]:
@@ -51,8 +63,6 @@ for _ in range(varLines):
         continue
     else:
         break
-labels = {}
-encounteredErrors = []
 
 for i, line in enumerate(assemblyCode, 1):
     if not line:
@@ -64,8 +74,37 @@ for i, line in enumerate(assemblyCode, 1):
             encounteredErrors.append(f"ERROR at Line {i + varLines}: Multiple declarations for the same label found")
             continue
         else:
-            labels[label] = binary8bit(i-1)
+            labels[label] = dec_to_bin(i - 1)
             line = rest_of_line.strip()
 
     currentLine = line.split()
-    # process the rest of the line here
+
+if currentLine[0] == "mov":
+    if len(currentLine) == 3:
+        reg1 = registerAddress(currentLine[1])
+        reg2 = registerAddress(currentLine[2])
+        if reg1 != -1 and reg2 != -1:
+            if reg1 == "111":
+                encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Illegal use of FLAGS")
+                continue
+            convertedBinary.append(opcode(currentLine[0], 1) + "00000" + reg1 + reg2)
+            continue
+        elif currentLine[2][1:].isdecimal():
+            if currentLine[2][0] != "$":
+                encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Syntax Error")
+                continue
+            if reg1 == -1:
+                encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Invalid Register")
+                continue
+            if reg1 == "111":
+                encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Illegal use of FLAGS")
+                continue
+            imm_value = int(currentLine[2][1:])
+            if imm_value < 0 or imm_value > 255:
+                encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Illegal Immediate Value")
+                continue
+            convertedBinary.append(opcode(currentLine[0], 0) + reg1 + binary8bit(imm_value))
+            continue
+    encounteredErrors.append("ERROR at Line " + str(lineNumber + varLines + 1) + ": Wrong Syntax used for instruction")
+    continue
+
